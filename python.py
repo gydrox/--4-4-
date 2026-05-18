1519f515b49cbed6f53549d3c93fb3ab79d74a9d
gydrox <kokimashihara@gmail.com>
2026-05-18 15:35:59 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,6 @@
+book_name = input("Введите название любимой книги: ")
+genre = input("Введите жанр книги: ")
+author = input("Введите автора в родительном падеже:")
+
+sentence = "Моя любимая книга - " + genre + " " + author + " <<" + book_name + ">>."
+print(sentence)
